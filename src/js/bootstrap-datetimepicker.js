fe99214d2b6c2e661919015c7778361fdb9fa437
deostroll <deostroll@gmail.com>
2017-03-20 21:40:05 +0530

    modified logic in setInlineView

--- src/js/bootstrap-datetimepicker.js
+++ src/js/bootstrap-datetimepicker.js
@@ -2340,13 +2340,15 @@
                     //show the date picker
                     cache.firstLi.collapse('show');
                     cache.lastLi.collapse('hide');
-                    cache.datepickerDivs.hide().find('.datepicker-days').hide();
+                    cache.datepickerDivs.hide();
+                    $('.datepicker-days', cache.datepickerDivs).show();
                     cache.span.removeClass('glyphicon-calendar').addClass('glyphicon-time');
                 } else if (view === 'timepicker') {
                     //show the time picker
                     cache.firstLi.collapse('hide');
                     cache.lastLi.collapse('show');
-                    cache.timepickerDivs.hide().find('.timepicker-picker').show();
+                    cache.timepickerDivs.hide();
+                    $('.timepicker-picker', cache.timepickerDivs).show();
                     cache.span.removeClass('glyphicon-time').addClass('glyphicon-calendar');
                 }
             }
